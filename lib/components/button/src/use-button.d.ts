8737d7a741e0321c5128f9d4e73fcc1ddcfa3c2c
dmix <daniel.p.mcgrady@gmail.com>
2023-02-19 12:08:39 -0500

    first commit

---
+++ lib/components/button/src/use-button.d.ts
@@ -0,0 +1,11 @@
+import type { SetupContext } from 'vue';
+import type { ButtonEmits, ButtonProps } from './button';
+export declare const useButton: (props: ButtonProps, emit: SetupContext<ButtonEmits>['emit']) => {
+    _disabled: import("vue").ComputedRef<boolean>;
+    _size: import("vue").ComputedRef<"" | "default" | "small" | "large">;
+    _type: import("vue").ComputedRef<"" | "default" | "primary" | "success" | "warning" | "info" | "danger" | "text">;
+    _ref: import("vue").Ref<HTMLButtonElement | undefined>;
+    shouldAddSpace: import("vue").ComputedRef<boolean>;
+    tag: import("vue").ComputedRef<string>;
+    handleClick: (evt: MouseEvent) => void;
+};
